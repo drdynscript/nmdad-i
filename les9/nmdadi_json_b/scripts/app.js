d1083661701d105e5613afe2fb116581ebec28ee
unknown <Administrator@DMARA12--8754W7.student.arteveldehs.be>
2013-11-27 13:13:41 +0100

    Adding Les9 - Groeb b - update

---
+++ les9/nmdadi_json_b/scripts/app.js
@@ -0,0 +1,25 @@
+/*
+    Make a new JSON file
+    ====================
+    DataTypes
+    ---------
+    1) Number
+        double precision floating point value
+        ex.: 16.54
+    2) String
+        double quoted --> "..."
+        backslash escaping --> "\"...\""
+    3) Boolean
+        true | false
+    4) Array
+        ordered comma-separated sequence of values
+        enclosed by square brackets --> [...]
+        datatype of values can be different
+    5) null
+        empty
+    6) Object
+        unordered comma-separated collection of key/value pairs
+        enclosed by curly braces --> {...}
+        key is always a string without backslash escaping
+        separation between key and value by double point --> :
+*/
